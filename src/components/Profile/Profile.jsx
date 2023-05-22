1c3b2ded6ad32f589ecfce02adc296b7e9f885ab
skm95 <59934627+skm95@users.noreply.github.com>
2023-05-22 16:48:07 -0400

    Adding component and page for main profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import profile_pic from '../../assets/images/profile-pic.jpg'
+
+const Profile = () => {
+    return (
+        <div className="flex items-center font-josefin justify-center">
+            <img src={profile_pic} alt="profile" className="w-3/12 mr-16 border-dark-green border-4 rounded-lg"/>
+            <div className="text-left w-5/12">
+                <p>Welcome to my page!</p>
+                <br />
+                <p>
+                    My name is Sarah Moreland and I am currently living in Boston and working as an Associate Consultant at Slalom Consulting.
+                    I'm a part of their Cloud Residency Program that focuses on the AWS Cloud. 
+                    Since starting the program, I have completed a rigorous training program and received the associate level 
+                    AWS certifications for Solution Architect, Developer, and SysOps. 
+                    Currently I am gaining client experience and working on expanding my front-end and AWS knowledge.
+                </p>
+                <br />
+                <p>
+                    Outside of work, I enjoy playing music with my friends. I am currently working on the electric guitar but I also love sitting down
+                    at my keyboard when I have time. I try and get out and see live concerts as well. I have been really enjoying the Boston music scene.
+                    I also enjoy watching movies when I have the time. Feel free to check out my LetterBoxd!
+                </p>
+            </div>
+            
+        </div>
+    );
+  }
+  
+  export default Profile
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     fontFamily: {
-      bebas: ['Bebas Neue', 'Helvetica', 'Arial','sans-serif']
+      bebas: ['Bebas Neue', 'Helvetica', 'Arial','sans-serif'],
+      josefin: ['Josefin Sans','sans-serif'],
     },
     extend: {
       colors: {
         'light-green': '#CCEE8E',
-        'dark-green': '#3A451F'
+        'dark-green': '#3A451F',
+        'tan-green': '#F0F3E5'
       }
     },
   },
