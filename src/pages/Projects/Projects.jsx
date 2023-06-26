ba3f949d608c718b66d666a7276e820dfe033708
skm95 <59934627+skm95@users.noreply.github.com>
2023-06-26 17:31:18 -0400

    Adding certifications

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,10 @@
+import Certifications from '../../components/Certifications/Certifications';
 import CodingProjects from '../../components/CodingProjects/CodingProjects';
 import ProfessionalExperiences from '../../components/ProfessionalExperiences/ProfessionalExperiences';
 function Projects() {
     return (
         <div className="Home h-screen bg-white py-16 space-y-8">
+            <Certifications />
             <ProfessionalExperiences />
             <CodingProjects />
         </div>
