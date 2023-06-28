b2d337303b0424ec852359a4f6b8b6ba80b1a7bc
skm95 <59934627+skm95@users.noreply.github.com>
2023-06-28 11:38:37 -0400

    Updating layout of experience page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,11 +3,16 @@ import CodingProjects from '../../components/CodingProjects/CodingProjects';
 import ProfessionalExperiences from '../../components/ProfessionalExperiences/ProfessionalExperiences';
 function Projects() {
     return (
-        <div className="Home h-screen bg-white py-16 space-y-8 pb-12">
-            <Certifications />
-            <ProfessionalExperiences />
-            <CodingProjects />
-            <div className="pb-8" />
+        <div className="Home h-screen bg-white py-16 flex justify-center">
+            <div className="flex flex-col space-y-8">
+                <ProfessionalExperiences />
+                <div className="pb-8">
+                    <CodingProjects />
+                </div>
+            </div>
+            <div className="pr-8">
+                <Certifications />
+            </div>
         </div>
   );
 }
