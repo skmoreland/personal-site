3b4eb43a17a540c8c794c076f7f011568ec0108c
Sarah Moreland <sarahmoreland@Sarahs-MacBook-Air.local>
2023-07-05 10:52:52 -0400

    Adding styling with prime colors

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,14 +3,14 @@ import CodingProjects from '../../components/CodingProjects/CodingProjects';
 import ProfessionalExperiences from '../../components/ProfessionalExperiences/ProfessionalExperiences';
 function Projects() {
     return (
-        <div className="Home h-screen bg-white py-16 flex justify-center">
+        <div className="Home py-16 flex justify-center">
             <div className="flex flex-col space-y-8">
                 <ProfessionalExperiences />
                 <div className="pb-8">
                     <CodingProjects />
                 </div>
             </div>
-            <div className="pr-8">
+            <div className="pr-8 w-1/5">
                 <Certifications />
             </div>
         </div>
