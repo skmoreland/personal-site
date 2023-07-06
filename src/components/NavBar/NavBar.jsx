a619bba09e6e7f7246751c385ab8bd53b379d449
Sarah Moreland <sarahmoreland@Sarahs-MacBook-Air.local>
2023-07-06 09:51:43 -0400

    Linking about me page to my name

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
       <nav className = "bg-black w-screen h-16 flex justify-between items-center px-4 font-title text-white">
-            <div className = "text-2xl lg:text-3xl" >Sarah Moreland</div>
+            <div className = "text-2xl lg:text-3xl" >
+              <Link to="/">Sarah Moreland</Link>
+            </div>
             <div className = "flex justify-between gap-x-4 decoration-4 underline-offset-4 lg:text-xl">
               <Link to="/" className="decoration-prime-red hover:underline hover:underline">About Me</Link>
               <Link to="/projects" className="decoration-prime-blue hover:underline">Experience</Link>
